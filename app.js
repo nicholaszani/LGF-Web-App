a44abc216189670493bdde3c1a30422d3a2045c6
Nicholas Zani <nczani@gmail.com>
2022-06-09 20:27:12 -0300

    add 1 table for each equipment, and add equipment selector

--- app.js
+++ app.js
@@ -9,31 +9,109 @@ app.set('view engine', 'ejs');
 mongoose.connect("mongodb://localhost:27017/reservasDB");
 
 
+//MONGODB
+const bookingSchema = new mongoose.Schema ({
+  dia: String,
+  horario: String,
+  nome: String,
+  equipamento: String
+});
+
+const Book = mongoose.model("Book", bookingSchema);
+
+
+
 // GET METHOD
 app.get("/", function(req, res){
-  res.render("home", {pageName: "Reserva de equipamentos"});
+  res.render("home", {titleName: "Reserva de equipamentos"});
 });
 
 app.get("/reservas", function(req, res){
-  res.render("reservas", {pageName: "Reservas"});
+  res.render("reservas", {titleName: "Reservas"});
 });
 
-app.get("/reservas/:customRouteName1", function(req, res){
-  const customRouteName1 = _.capitalize(req.params.customRouteName1);
-  res.render("reservaequipamentos", {pageName: "Reservas " + customRouteName1.replaceAll('-', ' '), tableName: customRouteName1.replaceAll('-', ' ')});
+app.get("/reservas/:customRouteName", function(req, res){
+  const customRouteName = req.params.customRouteName;
+  const titleName1 = _.capitalize(customRouteName);
+  const titleName2 = titleName1.replaceAll('-', ' ');
+  res.render("reservaequipamentos", {titleName: "Reservas " + titleName2, capPageName: titleName2, pageName: customRouteName});
 });
 
 app.get("/reservar", function(req, res){
-  res.render("reservar", {pageName: "Reservar"});
+  res.render("reservar", {titleName: "Reservar"});
 });
 
-app.get("/reservar/:customRouteName2", function(req, res){
-  const customRouteName2 = _.capitalize(req.params.customRouteName2);
-  res.render("reservaequipamentos", {pageName: "Reservar " + customRouteName2.replaceAll('-', ' '), tableName: customRouteName2.replaceAll('-', ' ')});
+app.get("/reservar/:customRouteName", function(req, res){
+  const customRouteName = req.params.customRouteName;
+  const titleName1 = _.capitalize(customRouteName);
+  const titleName2 = titleName1.replaceAll('-', ' ');
+  switch (customRouteName) {
+    case "capela":
+      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: ""});
+      break;
+    case "centrifugas":
+      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "CE-01", table2Name: "CE-02"});
+      break;
+    case "fluxo":
+      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "FL-01", table2Name: "FL-02"});
+      break;
+    case "leitor-de-placas":
+      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: ""});
+      break;
+    case "qpcr":
+      res.render("reservaequipamentos", {titleName: "Reservar qPCR", capPageName: "qPCR", pageName: customRouteName, table1Name: ""});
+      break;
+    case "shakers":
+      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "SH-01", table2Name: "SH-02"});
+      break;
+    case "termocicladores":
+      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "TC-01", table2Name: "TC-02", table3Name: "TC-03"});
+      break;
+    default: console.log("error");
+
+  }
 });
 
 
 
+//POST METHOD
+app.post("/reservar/:customRouteName2", function(req, res){
+  // const booked = new Book {
+  //   dia:
+  //   horario:
+  //   nome:
+  //   equipamento:
+  // }
+  // const customRouteName2 = _.lowerCase(req.params.customRouteName2);
+  // switch (customRouteName2) {
+  //   case "capela":
+  //
+  //     break;
+  //   case "centrifugas":
+  //
+  //     break;
+  //   case "fluxo":
+  //
+  //     break;
+  //   case "leitor de placas":
+  //
+  //     break;
+  //   case "qpcr":
+  //
+  //     break;
+  //   case "shakers":
+  //
+  //     break;
+  //   case "termocicladores":
+  //
+  //     break;
+  //   default: console.log("error");
+  // }
+});
+
+
+
+//CONNECTION
 app.listen(3000, function(){
   console.log("Server is up and running on port 3000");
 });
---
+++ public/script/index.js
@@ -0,0 +1,1 @@
+const tableDiv = $("div.reserveTable");
