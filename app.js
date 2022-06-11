5c3d446a96428d5c710cafd6d9411a5c1138ca3f
Nicholas Zani <nczani@gmail.com>
2022-06-11 12:47:59 -0300

    finish frontend, continue backend development.

--- app.js
+++ app.js
@@ -34,7 +34,31 @@ app.get("/reservas/:customRouteName", function(req, res){
   const customRouteName = req.params.customRouteName;
   const titleName1 = _.capitalize(customRouteName);
   const titleName2 = titleName1.replaceAll('-', ' ');
-  res.render("reservaequipamentos", {titleName: "Reservas " + titleName2, capPageName: titleName2, pageName: customRouteName});
+  switch (customRouteName) {
+    case "capela":
+      res.render("equipamentos-reservados", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: ""});
+      break;
+    case "centrifugas":
+      res.render("equipamentos-reservados", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "CE-01", table2Name: "CE-02"});
+      break;
+    case "fluxo":
+      res.render("equipamentos-reservados", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "FL-01", table2Name: "FL-02"});
+      break;
+    case "leitor-de-placas":
+      res.render("equipamentos-reservados", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: ""});
+      break;
+    case "qpcr":
+      res.render("equipamentos-reservados", {titleName: "Reservar qPCR", capPageName: "qPCR", pageName: customRouteName, table1Name: ""});
+      break;
+    case "shakers":
+      res.render("equipamentos-reservados", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "SH-01", table2Name: "SH-02"});
+      break;
+    case "termocicladores":
+      res.render("equipamentos-reservados", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "TC-01", table2Name: "TC-02", table3Name: "TC-03"});
+      break;
+    default: console.log("error");
+
+  }
 });
 
 app.get("/reservar", function(req, res){
@@ -47,25 +71,25 @@ app.get("/reservar/:customRouteName", function(req, res){
   const titleName2 = titleName1.replaceAll('-', ' ');
   switch (customRouteName) {
     case "capela":
-      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: ""});
+      res.render("reservar-equipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: ""});
       break;
     case "centrifugas":
-      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "CE-01", table2Name: "CE-02"});
+      res.render("reservar-equipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "CE-01", table2Name: "CE-02"});
       break;
     case "fluxo":
-      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "FL-01", table2Name: "FL-02"});
+      res.render("reservar-equipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "FL-01", table2Name: "FL-02"});
       break;
     case "leitor-de-placas":
-      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: ""});
+      res.render("reservar-equipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: ""});
       break;
     case "qpcr":
-      res.render("reservaequipamentos", {titleName: "Reservar qPCR", capPageName: "qPCR", pageName: customRouteName, table1Name: ""});
+      res.render("reservar-equipamentos", {titleName: "Reservar qPCR", capPageName: "qPCR", pageName: customRouteName, table1Name: ""});
       break;
     case "shakers":
-      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "SH-01", table2Name: "SH-02"});
+      res.render("reservar-equipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "SH-01", table2Name: "SH-02"});
       break;
     case "termocicladores":
-      res.render("reservaequipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "TC-01", table2Name: "TC-02", table3Name: "TC-03"});
+      res.render("reservar-equipamentos", {titleName: "Reservar " + titleName2, capPageName: titleName2, pageName: customRouteName, table1Name: "TC-01", table2Name: "TC-02", table3Name: "TC-03"});
       break;
     default: console.log("error");
 
@@ -75,38 +99,44 @@ app.get("/reservar/:customRouteName", function(req, res){
 
 
 //POST METHOD
-app.post("/reservar/:customRouteName2", function(req, res){
-  // const booked = new Book {
-  //   dia:
-  //   horario:
-  //   nome:
-  //   equipamento:
-  // }
-  // const customRouteName2 = _.lowerCase(req.params.customRouteName2);
-  // switch (customRouteName2) {
-  //   case "capela":
-  //
-  //     break;
-  //   case "centrifugas":
-  //
-  //     break;
-  //   case "fluxo":
-  //
-  //     break;
-  //   case "leitor de placas":
-  //
-  //     break;
-  //   case "qpcr":
-  //
-  //     break;
-  //   case "shakers":
-  //
-  //     break;
-  //   case "termocicladores":
-  //
-  //     break;
-  //   default: console.log("error");
-  // }
+app.post("/reservar/:customRouteName", function(req, res){
+  const customRouteName2 = _.lowerCase(req.params.customRouteName);
+  const diaReservado = req.body.dias;
+  const horarioReservado = req.body.horarios;
+  const nomeReservado = req.body.nomes;
+  const equipamentoReservado = req.body.equipamentos;
+
+  const booked = new Book {
+    dia: diaReservado,
+    horario: horarioReservado,
+    nome: nomeReservado,
+    equipamento: equipamentoReservado
+  }
+
+  switch (customRouteName) {
+    case "capela":
+      
+      break;
+    case "centrifugas":
+
+      break;
+    case "fluxo":
+
+      break;
+    case "leitor de placas":
+
+      break;
+    case "qpcr":
+
+      break;
+    case "shakers":
+
+      break;
+    case "termocicladores":
+
+      break;
+    default: console.log("error");
+  }
 });
 
 
